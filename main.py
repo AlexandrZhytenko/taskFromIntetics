caa22c9ea6d69c40f94bf062fab9071166cedf3a
Olexandr Zhytenko <zhytenko@ukr.net>
2018-03-03 02:12:12 +0200

    first commit

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from sympy import sieve
-from sympy.utilities.iterables import multiset_combinations
-from numpy import prod
-
-def is_palindrome(number):
-    s = str(number)
-    return s == s[::-1]
+from itertools import combinations_with_replacement
 
 def max_palindrome(prime_numbers):
-    result = []
-    prime_numbers_list_unique_combinations = tuple(multiset_combinations(prime_numbers, 2))
-    product_for_each_pair = prod(prime_numbers_list_unique_combinations, axis=1)
-    for i in product_for_each_pair:
-        if is_palindrome(i):
-            result.append((i, prime_numbers_list_unique_combinations[product_for_each_pair.tolist().index(i)]))
-    return "The max polindrome is {0} which is producted of {1} and {2} numbers.".format(max(result)[0], \
-                                                                                                max(result)[1][0], \
-                                                                                                max(result)[1][1])
+    prime_numbers_list_unique_combinations = combinations_with_replacement(prime_numbers, 2)
+    list_palindromes = ((i * j, i, j) for i, j in prime_numbers_list_unique_combinations if \
+                        str(i * j) == str(i * j)[::-1])
+    result = max(list_palindromes)
+    return "The max palindrome with product numbers {1} and {2} is: {0}".format(result[0], \
+                                                                                 result[1], \
+                                                                                 result[2])
 
-prime_numbers = (i for i in sieve.primerange(9999, 99999))
-print max_palindrome(prime_numbers)
+from timeit import default_timer
+start = default_timer()
+
+print max_palindrome((i for i in sieve.primerange(9999, 99999)))
+# The max palindrome with product numbers 30109 and 33211 is: 999949999
+
+end = default_timer()
+print "The time of max_palindrome program's execution is {} sec".format(end - start)
+# The time of max_palindrome program's execution is 60.7272051733 sec
